50ede7501b1148cad3c2e767d62454c72aff9995
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-11-02 13:30:41 +0530

    Db configuration

--- OrderService/src/main/java/org/cripsy/orderservice/OrderServiceApplication.java
+++ OrderService/src/main/java/org/cripsy/orderservice/OrderServiceApplication.java
@@ -1,13 +1,22 @@
 package org.cripsy.orderservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
--- ProductService/src/main/java/org/cripsy/productservice/ProductServiceApplication.java
+++ ProductService/src/main/java/org/cripsy/productservice/ProductServiceApplication.java
@@ -1,13 +1,22 @@
 package org.cripsy.productservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
