eea0eb2e0fb113fff1524938dd4c6e129a384f26
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-11-02 23:38:43 +0530

    All services Done

--- DeliveryService/src/main/java/org/cripsy/deliveryservice/DeliveryServiceApplication.java
+++ DeliveryService/src/main/java/org/cripsy/deliveryservice/DeliveryServiceApplication.java
@@ -1,4 +1,4 @@
-package org.cripsy.chatservice;
+package org.cripsy.deliveryservice;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class ChatServiceApplication {
+public class DeliveryServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChatServiceApplication.class, args);
+        SpringApplication.run(DeliveryServiceApplication.class, args);
     }
 
     @Bean
