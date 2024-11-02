a73d2f3e2a7c950aaecf1138688acbd2e92768ce
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-11-02 10:53:05 +0530

    File Structure

--- UserService/src/main/java/org/cripsy/userservice/UserServiceApplication.java
+++ UserService/src/main/java/org/cripsy/userservice/UserServiceApplication.java
@@ -1,13 +1,22 @@
 package org.cripsy.userservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
