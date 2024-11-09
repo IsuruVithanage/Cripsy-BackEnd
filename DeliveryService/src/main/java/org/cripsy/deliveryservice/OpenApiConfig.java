4c35a1b0868125bf4ac65af18ca10484f511491e
AshanSalinda <ashansalinda5@gmail.com>
2024-11-09 14:58:29 +0530

    Api Documentation for All Services

---
+++ DeliveryService/src/main/java/org/cripsy/deliveryservice/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package org.cripsy.deliveryservice;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title(applicationName)
+                .version("v1.0")
+                .description("This is the API documentation for the " + applicationName));
+    }
+}
