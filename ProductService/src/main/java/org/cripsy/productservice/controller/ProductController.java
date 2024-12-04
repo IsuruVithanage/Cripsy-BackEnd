7c165f89c03851ab1fa5ea0dd2ae963685042c94
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-12-05 01:31:24 +0530

    Product load

--- ProductService/src/main/java/org/cripsy/productservice/controller/ProductController.java
+++ ProductService/src/main/java/org/cripsy/productservice/controller/ProductController.java
@@ -21,18 +21,17 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
-    /*@GetMapping("/getAllProductDetail")
-    @Operation(summary = "Get All Products details", description = "Fetch a list of all available products details.", tags = "Product")
-    public List<UpdateProductDTO> getAllProductDetails() {
-        return productService.getAllProductDetails();
-    }*/
+    @GetMapping("/getAllProductsDetails")
+    @Operation(summary = "Get All Products", description = "Fetch a list of all available products.", tags = "Product")
+    public List<UpdateProductDTO> getAllProductsDetails() {
+        return productService.getAllProductsDetails();
+    }
 
     @GetMapping("/{productId}/{userId}")
     @Operation(summary = "Get a Product", description = "Fetch the details of a single product.", tags = "Product")
     public ProductItemDTO getProductById(@PathVariable Integer productId, @PathVariable Integer userId){
         return productService.getProductById(productId, userId);
     }
-
 
     @GetMapping("/reviews/{productId}/{pageNo}")
     @Operation(summary = "Get Reviews", description = "Fetch a List of Reviews of a product with pagination", tags = "Reviews")
