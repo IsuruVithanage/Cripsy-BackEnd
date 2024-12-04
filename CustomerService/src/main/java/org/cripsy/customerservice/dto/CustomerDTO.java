4b1367772a2bf7c582695b1a6e986aed69344c77
Divyanjali Palpola <palpolatbd.21@uom.lk>
2024-12-04 23:11:11 +0530

    Update customer service

--- CustomerService/src/main/java/org/cripsy/customerservice/dto/CustomerDTO.java
+++ CustomerService/src/main/java/org/cripsy/customerservice/dto/CustomerDTO.java
@@ -9,8 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CustomerDTO {
     private Long id;
-    private String firstName;
-    private String lastName;
+    private String userName;
     private String email;
     private String password;
 }
