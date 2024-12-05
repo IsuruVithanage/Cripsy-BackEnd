a05c8c70b44910621662da52a7948f4fc652650b
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-12-06 02:10:56 +0530

    Product update

--- AuthenticationService/src/main/java/org/cripsy/authenticationservice/controller/AuthController.java
+++ AuthenticationService/src/main/java/org/cripsy/authenticationservice/controller/AuthController.java
@@ -1,7 +1,7 @@
 package org.cripsy.authenticationservice.controller;
 
-import org.cripsy.customerservice.dto.AuthDTO;
 import org.cripsy.authenticationservice.service.AuthService;
+import org.cripsy.customerservice.dto.AuthDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,16 +20,4 @@ public class AuthController {
         return authService.createUser(authDTO);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody Map<String, String> loginRequest) {
-        String username = loginRequest.get("username");
-        String password = loginRequest.get("password");
-
-        try {
-            String token = authService.login(username, password);
-            return ResponseEntity.ok(token);
-        } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-        }
-    }
 }
--- AuthenticationService/src/main/java/org/cripsy/authenticationservice/service/AuthService.java
+++ AuthenticationService/src/main/java/org/cripsy/authenticationservice/service/AuthService.java
@@ -3,7 +3,6 @@ package org.cripsy.authenticationservice.service;
 import org.cripsy.customerservice.dto.AuthDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
